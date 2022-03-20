87d86f2cea2076336a5976311fca11bed0456d1c
Nika <nika.tskhadaia@gmail.com>
2022-03-20 18:00:03 +0400

    initial

---
+++ Consumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+//var factory = new ConnectionFactory() { HostName = "localhost" };
+var factory = new ConnectionFactory() { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+
+using (var connection = factory.CreateConnection()) 
+{
+    using var channel = connection.CreateModel();
+    channel.QueueDeclare(queue: "my-first-queue",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" Message Received {0}", message);
+    };
+    channel.BasicConsume(queue: "my-first-queue",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.ReadLine();
+}
---
+++ Producer/RabbitMQProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+//var factory = new ConnectionFactory() { HostName = "localhost" };
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("my-first-queue",
+durable: true,
+exclusive: false,
+autoDelete: false,
+arguments: null);
+
+
+
+var thismessage = Console.ReadLine() ?? "Hello from docker";
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(thismessage));
+
+
+
+channel.BasicPublish("", "my-first-queue", null, body);
+
+Console.ReadLine();
